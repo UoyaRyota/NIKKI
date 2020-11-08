bf42cdafb36b10e71cea2f46084702984172e136
uoyaryota <lestwinsdance8@gmail.com>
2020-11-08 22:24:22 +0800

    adjust css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,12 @@
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.copy.css') }}" rel="stylesheet">
+    
     <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
-    <link rel="stylesheet" type="text/css" href="https://nikki-eigo-app.herokuapp.com/css/app.css">
-    <link rel="stylesheet" type="text/css" href="https://nikki-eigo-app.herokuapp.com/css/app.copy.css">
+    <!-- <link rel="stylesheet" type="text/css" href="https://nikki-eigo-app.herokuapp.com/css/app.css">
+    <link rel="stylesheet" type="text/css" href="https://nikki-eigo-app.herokuapp.com/css/app.copy.css"> -->
     <!-- <link href="{{ asset('css/app.copy.css') }}" rel="stylesheet"> -->
 
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
